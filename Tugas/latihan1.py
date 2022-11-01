892865e77c85eaa70f4766a00fc0b316ddd89655
Rico Prasetya <ricoprasetya24@gmail.com>
2022-11-01 10:34:34 +0700

    menambah tugas

--- Tugas/latihan1.py
+++ Tugas/latihan1.py
@@ -1,1 +1,7 @@
-“# latihan1”
+bil_1=input("Masukkan Bilangan Ke-1 : ")
+bil_2=input("Masukkan Bilangan Ke-2 : ")
+print()
+if bil_1 > bil_2:
+    print("Bilangan Terbesar : ",bil_1)
+elif bil_1 < bil_2:
+    print("Bilangan Terbesar : ",bil_2)
