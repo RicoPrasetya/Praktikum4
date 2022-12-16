6392a874c7373735f886b1f82fe164e190fd9e0a
Rico Prasetya <ricoprasetya24@gmail.com>
2022-12-16 15:41:00 +0700

    menambah tugas

--- Tugas/latihan3.py
+++ Tugas/latihan3.py
@@ -1,1 +1,12 @@
-
+i=-2
+for baris in range(10):
+  i=i+1
+  for kolom in range(10):
+    i=i+1
+    if i<10:
+      print(i,end='  ')
+    elif i>9:
+      print(i, end=' ')
+  else:
+    i=i-10
+    print('')
